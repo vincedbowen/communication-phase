22e631d475267131550c2cc4762240676ad65aa8
Vincent Bowen <vibo3564@colorado.edu>
2023-11-13 11:52:32 -0700

    trying trailing slash fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'vincedbowen', // Usually your GitHub org/user name.
   projectName: 'communication-phase', // Usually your repo name.
-  trailingSlash: true,
+  trailingSlash: false,
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
